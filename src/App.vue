b0c0ed2d962eb50ce94e52dfb29f26b118883032
江鹏利 <845415120@qq.com>
2023-12-12 14:36:40 +0800

    vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
+  <el-button type="primary" round>Primary</el-button>
   <router-view />
 </template>
 
